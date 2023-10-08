2c21231326ad041a0a2538f34423387c9ff54d76
chryl <cyl5yy412@gmail.com>
2023-10-08 13:28:52 +0800

    读取excel文件转为json

---
+++ src/main/java/com/chryl/po/OrgDTO.java
@@ -0,0 +1,17 @@
+package com.chryl.po;
+
+import lombok.Data;
+
+/**
+ * Created by Chr.yl on 2023/10/8.
+ *
+ * @author Chr.yl
+ */
+@Data
+public class OrgDTO {
+    private String orgCode;
+    private String orgName;
+    private String areaCode;
+    private String yhbhPrefix8;
+
+}
